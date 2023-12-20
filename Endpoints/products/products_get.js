36eaa7b73acbe76ee09dff7fa21bdd9ea07360a3
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-12-20 13:34:07 +0000

    added gelato api support

--- Endpoints/products/products_get.js
+++ Endpoints/products/products_get.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello products!')
+app.get('/', async (req, res) => {
+  const response = await fetch('https://product.gelatoapis.com/v3/catalogs', {headers: {'X-API-Key': process.env.GELATO_API_KEY}})
+  const products_data = await response.json()
+  res.send(products_data)
 })
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const products_router = require('./Endpoints/products/products_get.js')
-
+const dotenv = require('dotenv').config()
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -14,3 +14,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+// a221a169-528f-4728-9e9d-1ef39410bf6c-3c138c76-b210-43b7-91f7-1dab79fd42c4:3ce9745f-99fa-4746-a7a5-2cb1c0d8115c
